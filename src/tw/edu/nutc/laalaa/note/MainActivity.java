da57433d6558351db586e9ca68ab244a1eb8719b
Guanyuo <s11977037@gms.nutc.edu.tw>
2014-02-19 10:09:00 +0800

    新增測試Frac的Activity

--- src/tw/edu/nutc/laalaa/note/MainActivity.java
+++ src/tw/edu/nutc/laalaa/note/MainActivity.java
@@ -1,15 +1,82 @@
 package tw.edu.nutc.laalaa.note;
 
-import android.os.Bundle;
+import tw.edu.nutc.laalaa.note.views.FracCanvas;
+import tw.edu.nutc.laalaa.note.views.FracEditText;
+import tw.edu.nutc.laalaa.note.views.FracImageView;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class MainActivity extends Activity {
+
+	private LinearLayout mLayout;
+	private ScrollView mScrollView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		mLayout = (LinearLayout) findViewById(R.id.linear_layout);
+		mScrollView = (ScrollView) findViewById(R.id.scrollview);
+		final Context context = this;
+		
+		Button addEditText = (Button) findViewById(R.id.add_edittext);
+		addEditText.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mLayout.addView(new FracEditText(context));
+				mLayout.postDelayed(new Runnable() {
+					
+					@Override
+					public void run() {
+						mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
+					}
+				}, 50);
+			}
+		});
+		
+		Button addCanvas = (Button) findViewById(R.id.add_canvas);
+		addCanvas.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				FracCanvas newCanvas = new FracCanvas(context);
+				mLayout.addView(newCanvas);
+				mLayout.postDelayed(new Runnable() {
+					
+					@Override
+					public void run() {
+						mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
+					}
+				}, 50);
+			}
+		});
+		
+		Button addImageView = (Button) findViewById(R.id.add_image);
+		addImageView.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				FracImageView imageView = new FracImageView(context);
+				imageView.setImageResource(R.drawable.p1);
+				mLayout.addView(imageView);
+				mLayout.postDelayed(new Runnable() {
+					
+					@Override
+					public void run() {
+						mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
+					}
+				}, 50);
+			}
+		});
 	}
 
 	@Override
