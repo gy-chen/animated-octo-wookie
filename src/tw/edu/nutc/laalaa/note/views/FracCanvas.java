f56ec7d2ea410775a877b3d6fe715bf8ad31d37f
Guanyuo <gy.chen@gms.nutc.edu.tw>
2014-02-22 11:08:56 +0800

    FracCanvas   - 新增isDrawing()   - 新增OnDrawListener

--- src/tw/edu/nutc/laalaa/note/views/FracCanvas.java
+++ src/tw/edu/nutc/laalaa/note/views/FracCanvas.java
@@ -11,12 +11,22 @@ import android.util.Log;
 import android.view.MotionEvent;
 import android.widget.ImageView;
 
+/**
+ * FracCanvas
+ * 
+ * 可使用觸控繪圖的畫布
+ * 
+ * @author Guanyuo <gy.chen@gms.nutc.edu.tw>
+ *
+ */
 public class FracCanvas extends ImageView {
 
 	private final String TAG = "FracCanvas";
-	
+
 	private final LinkedList<Path> mPaths = new LinkedList<Path>();
 	private Path mCurrentPath = null;
+	private boolean mIsDrawing = false;
+	private OnDrawListener mOnDrawListener;
 
 	private Paint mPaint;
 
@@ -41,25 +51,60 @@ public class FracCanvas extends ImageView {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * 使用者是否正在此畫布繪圖
+	 * 
+	 * @return true代表是，否則為不是
+	 */
+	public boolean isDrawing() {
+		Log.d(TAG, "Check Drawing status " + mIsDrawing);
+		return mIsDrawing;
+	}
+	
+	/**
+	 * 新增繪圖事件Listener
+	 * 
+	 * 當有繪圖事件發生時，會通知此Listener
+	 * 
+	 * @param listener
+	 */
+	public void setOnDrawListener(OnDrawListener listener) {
+		mOnDrawListener = listener;
+	}
+	
+	/**
+	 * 取得目前的繪圖事件Listener
+	 * 
+	 * 如果目前沒有listener存在，則回傳null
+	 * 
+	 * @return
+	 */
+	public OnDrawListener getOnDrawListener() {
+		return mOnDrawListener;
+	}
+
 	@Override
 	public void onDraw(Canvas canvas) {
 		super.onDraw(canvas);
-		Log.d(TAG, "onDraw");
 
 		canvas.drawColor(0xffffff66);
 		for (Path path : mPaths) {
 			canvas.drawPath(path, mPaint);
 		}
 	}
-	
+
 	@Override
 	public boolean onTouchEvent(MotionEvent event) {
 		Log.d(TAG, String.format("%s onTouchEvent", getId()));
-		
+
 		int action = event.getAction();
-		switch(action) {
+		switch (action) {
 		case MotionEvent.ACTION_DOWN:
 			if (mCurrentPath == null) {
+				mIsDrawing = true;
+				if (mOnDrawListener != null) {
+					mOnDrawListener.onStartDraw();
+				}
 				mCurrentPath = new Path();
 				mCurrentPath.moveTo(event.getX(), event.getY());
 				mPaths.add(mCurrentPath);
@@ -71,7 +116,10 @@ public class FracCanvas extends ImageView {
 			invalidate();
 			break;
 		case MotionEvent.ACTION_UP:
-		case MotionEvent.ACTION_CANCEL:
+			mIsDrawing = false;
+			if (mOnDrawListener != null) {
+				mOnDrawListener.onStopDraw();
+			}
 			mCurrentPath = null;
 			break;
 		}
@@ -86,5 +134,20 @@ public class FracCanvas extends ImageView {
 		Log.d(TAG, String.format("view size: (%d, %d", getWidth(), getHeight()));
 		setMeasuredDimension(getResources().getDisplayMetrics().widthPixels,
 				getResources().getDisplayMetrics().heightPixels / 4);
+	}
+
+	/**
+	 * OnDrawListener
+	 * 
+	 * onStartDraw()
+	 *   當使用者開始下筆時，會呼叫此方法
+	 * onStopDraw()
+	 *   當使用者停筆時，會呼叫此方法
+	 *   
+	 */
+	public static interface OnDrawListener {
+		void onStartDraw();
+
+		void onStopDraw();
 	}
 }
