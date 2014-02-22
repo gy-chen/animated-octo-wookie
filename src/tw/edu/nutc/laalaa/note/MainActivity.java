c1d2d9404757991edbe3f8a51467ff0dba590d7d
Guanyuo <gy.chen@gms.nutc.edu.tw>
2014-02-22 11:10:24 +0800

    - 測試新的畫布功能 - 新增可停止滑動的ScrollView

--- src/tw/edu/nutc/laalaa/note/MainActivity.java
+++ src/tw/edu/nutc/laalaa/note/MainActivity.java
@@ -1,10 +1,14 @@
 package tw.edu.nutc.laalaa.note;
 
+import java.util.ArrayList;
+
+import tw.edu.nutc.laalaa.note.utils.CustomScrollView;
 import tw.edu.nutc.laalaa.note.views.FracCanvas;
 import tw.edu.nutc.laalaa.note.views.FracEditText;
 import tw.edu.nutc.laalaa.note.views.FracImageView;
+import android.annotation.TargetApi;
 import android.app.Activity;
-import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -15,68 +19,129 @@ import android.widget.ScrollView;
 
 public class MainActivity extends Activity {
 
+	//private final String TAG = "NoteActivity";
+
 	private LinearLayout mLayout;
-	private ScrollView mScrollView;
+	private CustomScrollView mScrollView;
+	private ArrayList<Integer> mViewIds = new ArrayList<Integer>();
+	private FracCanvas.OnDrawListener mOnDrawListener;
+
+	protected final int TYPE_EDITTEXT = 1;
+	protected final int TYPE_CANVAS = 2;
+	protected final int TYPE_PHOTO = 3;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		// 當開始繪圖時，停止ScrollView滑動
+		mOnDrawListener = new FracCanvas.OnDrawListener() {
+			
+			@Override
+			public void onStopDraw() {
+				mScrollView.setScrollingEnabled(true);
+			}
+			
+			@Override
+			public void onStartDraw() {
+				mScrollView.setScrollingEnabled(false);				
+			}
+		};
+
 		mLayout = (LinearLayout) findViewById(R.id.linear_layout);
-		mScrollView = (ScrollView) findViewById(R.id.scrollview);
-		final Context context = this;
-		
+		mScrollView = (CustomScrollView) findViewById(R.id.scrollview);
+
 		Button addEditText = (Button) findViewById(R.id.add_edittext);
 		addEditText.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				mLayout.addView(new FracEditText(context));
-				mLayout.postDelayed(new Runnable() {
-					
-					@Override
-					public void run() {
-						mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
-					}
-				}, 50);
+				addView(TYPE_EDITTEXT);
 			}
 		});
-		
+
 		Button addCanvas = (Button) findViewById(R.id.add_canvas);
 		addCanvas.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				FracCanvas newCanvas = new FracCanvas(context);
-				mLayout.addView(newCanvas);
-				mLayout.postDelayed(new Runnable() {
-					
-					@Override
-					public void run() {
-						mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
-					}
-				}, 50);
+				addView(TYPE_CANVAS);
 			}
 		});
-		
+
 		Button addImageView = (Button) findViewById(R.id.add_image);
 		addImageView.setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
-				FracImageView imageView = new FracImageView(context);
-				imageView.setImageResource(R.drawable.p1);
-				mLayout.addView(imageView);
-				mLayout.postDelayed(new Runnable() {
-					
-					@Override
-					public void run() {
-						mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
-					}
-				}, 50);
+				addView(TYPE_PHOTO);
 			}
 		});
+	}
+
+	/**
+	 * 新增一個筆記物件至筆記簿
+	 * 
+	 * 丟入的筆記物件型態不合法時，丟出IllegalArgumentException
+	 * 
+	 * @param type 筆記物件型態
+	 */
+	protected void addView(int type) {
+		switch (type) {
+		case TYPE_EDITTEXT:
+			addEditText();
+			break;
+		case TYPE_CANVAS:
+			addCanvas();
+			break;
+		case TYPE_PHOTO:
+			addPhoto();
+			break;
+		default:
+			throw new IllegalArgumentException();
+		}
+	}
+
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+	protected void addEditText() {
+		FracEditText view = new FracEditText(this);
+		int viewId = View.generateViewId();
+		mViewIds.add(viewId);
+		view.setId(viewId);
+		addView(view);
+	}
+
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+	protected void addCanvas() {
+		FracCanvas view = new FracCanvas(this);
+		view.setOnDrawListener(mOnDrawListener);
+		int viewId = View.generateViewId();
+		mViewIds.add(viewId);
+		view.setId(viewId);
+		addView(view);
+	}
+
+	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+	protected void addPhoto() {
+		FracImageView view = new FracImageView(this);
+		view.setImageResource(R.drawable.p1);
+		int viewId = View.generateViewId();
+		mViewIds.add(viewId);
+		view.setId(viewId);
+		addView(view);
+	}
+
+	protected void addView(View view) {
+		mLayout.addView(view);
+		// 延遲時間讓ScrollView可滑動到底
+		mLayout.postDelayed(new Runnable() {
+
+			@Override
+			public void run() {
+				mScrollView.fullScroll(ScrollView.FOCUS_DOWN);
+			}
+		}, 50);
 	}
 
 	@Override
---
+++ src/tw/edu/nutc/laalaa/note/utils/CustomScrollView.java
@@ -0,0 +1,61 @@
+package tw.edu.nutc.laalaa.note.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.widget.ScrollView;
+
+public class CustomScrollView extends ScrollView {
+
+	// true if we can scroll the ScrollView
+	// false if we cannot scroll
+	private boolean scrollable = true;
+
+	public CustomScrollView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomScrollView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomScrollView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setScrollingEnabled(boolean scrollable) {
+		this.scrollable = scrollable;
+	}
+
+	public boolean isScrollable() {
+		return scrollable;
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent ev) {
+		switch (ev.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			// if we can scroll pass the event to the superclass
+			if (scrollable)
+				return super.onTouchEvent(ev);
+			// only continue to handle the touch event if scrolling enabled
+			return scrollable; // scrollable is always false at this point
+		default:
+			return super.onTouchEvent(ev);
+		}
+	}
+
+	@Override
+	public boolean onInterceptTouchEvent(MotionEvent ev) {
+		// Don't do anything with intercepted touch events if
+		// we are not scrollable
+		if (!scrollable)
+			return false;
+		else
+			return super.onInterceptTouchEvent(ev);
+	}
+
+}
