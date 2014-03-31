b3a82290cb1350f887ab2d39228e76f362b176c5
gy-chen <gy-chen@gms.nutc.edut.w>
2014-03-31 20:00:45 +0800

    FracImageView新增轉圈圈功能

--- src/tw/edu/nutc/laalaa/note/views/FracImageView.java
+++ src/tw/edu/nutc/laalaa/note/views/FracImageView.java
@@ -1,24 +1,70 @@
 package tw.edu.nutc.laalaa.note.views;
 
+import tw.edu.nutc.laalaa.note.R;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.util.AttributeSet;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class FracImageView extends ImageView {
 
+	private Animation mRotatingAnimation = AnimationUtils.loadAnimation(
+			getContext(), R.anim.rotate_around);
+
 	public FracImageView(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
+		init();
 	}
 
 	public FracImageView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
+		init();
 	}
 
 	public FracImageView(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
-		// TODO Auto-generated constructor stub
+		init();
 	}
-	
+
+	private void init() {
+		if (getDrawable() == null) {
+			startRotateAnimation();
+		}
+	}
+
+	public void startRotateAnimation() {
+		startAnimation(mRotatingAnimation);
+	}
+
+	public void stopRotateAnimation() {
+		mRotatingAnimation.cancel();
+	}
+
+	@Override
+	public void setImageBitmap(Bitmap bm) {
+		super.setImageBitmap(bm);
+		stopRotateAnimation();
+	}
+
+	@Override
+	public void setImageDrawable(Drawable drawable) {
+		super.setImageDrawable(drawable);
+		stopRotateAnimation();
+	}
+
+	@Override
+	public void setImageResource(int resId) {
+		super.setImageResource(resId);
+		stopRotateAnimation();
+	}
+
+	@Override
+	public void setImageURI(Uri uri) {
+		super.setImageURI(uri);
+		stopRotateAnimation();
+	}
 }
